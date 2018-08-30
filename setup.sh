f35ae0bfe7ffbfeebaf12b469043ac13e4ba99a1
Henry Franks <henryefranks@gmail.com>
2018-08-30 17:21:15 +0100

    Bug fixes

--- setup.sh
+++ setup.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 # Install NodeJS
-if [ ! -f "/etc/debian_version" ];
+if [ ! -f "/etc/debian_version" ]; then
    echo "error: this script only supports Debian versions 8 or 9"
+   exit 1
 fi
 
-sudo apt-get install curl software-properties-common less
+sudo apt-get install -y curl software-properties-common less
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
 sudo apt-get install -y nodejs
 
 # Install MongoDB
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 
-if [ "$(cat /etc/debian_version | head -c 1)" -eq "8" ]; then
+if [ "$(cat /etc/debian_version | head -c 1)" = "8" ]; then
 	echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-elif [ "$(cat /etc/debian_version | head -c 1)" -eq "9" ]; then
+elif [ "$(cat /etc/debian_version | head -c 1)" = "9" ]; then
 	echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 else
 	echo "error: unsupported version"
-	exit(1)
+	exit 1
 fi
 
 sudo apt-get update
@@ -33,14 +34,17 @@ echo "rs.initiate()" | mongo
 sudo systemctl enable mongod.service
 
 # Install Nginx
-sudo apt-get install nginx
+sudo apt-get install -y nginx
 
 # Run Nginx on startup
 sudo systemctl enable nginx
 
 # Copy files to appropriate locations
 sudo cp apollo-api /etc/nginx/sites-available/apollo-api
-ln -s /etc/nginx/sites-available/apollo-api /etc/nginx/sites-enabled/apollo-api
+sudo ln -s /etc/nginx/sites-available/apollo-api /etc/nginx/sites-enabled/apollo-api
+
+# Give NPM install permissions
+sudo chmod 777 /usr/lib/node_modules
 
 # Install PM2
 npm install pm2 -g
