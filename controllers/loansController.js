b728934dbcd49fa0c57a12f01075cf7ab0a5e923
Henry Franks <henryefranks@gmail.com>
2018-10-06 23:10:16 +0100

    Return book and user data from loans endpoints

--- controllers/loansController.js
+++ controllers/loansController.js
@@ -9,10 +9,49 @@ const	mongo		= require('../mongo'),
 		utils		= require('../utils');
 
 exports.getLoans = utils.asyncHandler(async (req, res) => {
-	res.json({
-		code: "000",
-		message: "Success",
-		data: await db.collection('loans').find().toArray()
+	const loans = await db.collection('loans').find().toArray();
+
+	let error = false;
+	const allData = loans.map(async loan => {
+		const withdraw = await db.collection('history').findOne({_id: loan.withdrawID});
+		if (!withdraw) {
+			utils.logError("Withdrawal '" + loan.withdrawID + "' not found");
+			error = true;
+		}
+
+		const book = await db.collection('books').findOne({_id: withdraw.book});
+		if (!book) {
+			utils.logError("Book '" + withdraw.book + "' not found");
+			error = true;
+		}
+
+		const user = await db.collection('users').findOne({_id: withdraw.user});
+		if (!user) {
+			utils.logError("User '" + withdraw.user + "' not found");
+			error = true;
+		}
+
+		return {
+			book: book,
+			user: user
+		}
+	});
+
+	if (error) {
+		res.json({
+			code: "001",
+			message: "Couldn't get loans"
+		});
+		return;
+	}
+
+	Promise.all(allData).then(data => {
+		res.json({
+			code: "000",
+			message: "Success",
+			count: data.length,
+			data: data
+		});
 	});
 });
 
@@ -22,9 +61,47 @@ exports.getOverdueLoans = utils.asyncHandler(async (req, res) => {
 	var now = new Date();
 	now.setHours(0,0,0,0);
 	const overdue = loans.filter(loan => !loan.returnDate && loan.dueDate < now);
-	res.json({
-		code: "000",
-		message: "Success",
-		data: overdue
+
+	let error = false;
+	const allData = overdue.map(async loan => {
+		const withdraw = await db.collection('history').findOne({_id: loan.withdrawID});
+		if (!withdraw) {
+			utils.logError("Withdrawal '" + loan.withdrawID + "' not found");
+			error = true;
+		}
+
+		const book = await db.collection('books').findOne({_id: withdraw.book});
+		if (!book) {
+			utils.logError("Book '" + withdraw.book + "' not found");
+			error = true;
+		}
+
+		const user = await db.collection('users').findOne({_id: withdraw.user});
+		if (!user) {
+			utils.logError("User '" + withdraw.user + "' not found");
+			error = true;
+		}
+
+		return {
+			book: book,
+			user: user
+		}
+	});
+
+	if (error) {
+		res.json({
+			code: "001",
+			message: "Couldn't get loans"
+		});
+		return;
+	}
+
+	Promise.all(allData).then(data => {
+		res.json({
+			code: "000",
+			message: "Success",
+			count: data.length,
+			data: data
+		});
 	});
 });
