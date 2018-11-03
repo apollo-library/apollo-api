8b78dedc011e24be02311488ed6f168cf8103045
Henry Franks <henryefranks@gmail.com>
2018-11-03 22:01:22 +0000

    Bug fixes

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -11,23 +11,7 @@ exports.getUsers = utils.asyncHandler(async (req, res) => {
 	res.json({
 		code: "000",
 		message: "Success",
-		data: await Promise.all((await db.collection('users').find().toArray()).map(async user => {
-			if (user.loanIDs && user.loanIDs.length) {
-				const loans = await utils.getLoansForIDs(user.loanIDs, db);
-
-				if (!loans) {
-					res.json({
-						code: "001",
-						message: "Couldn't get loans"
-					});
-					return;
-				}
-
-				user.loans = loans;
-				delete user.loanIDs;
-			}
-			return user;
-		}))
+		data: await db.collection('users').find().toArray()
 	});
 });
 
